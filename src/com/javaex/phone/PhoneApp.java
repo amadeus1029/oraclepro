2d0f8db5515f141d4e74098036b43b2439d74617
이웅희 <amadeus1029@gmail.com>
2020-06-17 15:52:34 +0900

    필요 메소드 작성 및 실험 완료

--- src/com/javaex/phone/PhoneApp.java
+++ src/com/javaex/phone/PhoneApp.java
@@ -1,4 +1,28 @@
 package com.javaex.phone;
 
+import java.util.List;
+
 public class PhoneApp {
+    public static void main(String[] args) {
+        PhoneDao phoneDao = new PhoneDao();
+        List<PersonVo> personList = phoneDao.getPersonList();
+
+        PersonVo person01 = new PersonVo("이효리","010-1111-1111","02-1111-1111");
+        PersonVo person02 = new PersonVo("정우성","010-2222-2222","02-2222-2222");
+        PersonVo person03 = new PersonVo("유재석","010-3333-3333","02-3333-3333");
+        PersonVo person04 = new PersonVo("이정재","010-4444-4444","02-4444-4444");
+        PersonVo person05 = new PersonVo("서장훈","010-5555-5555","02-5555-5555");
+
+        phoneDao.personInsert(person01);
+        phoneDao.personInsert(person02);
+        phoneDao.personInsert(person03);
+        phoneDao.personInsert(person04);
+        phoneDao.personInsert(person05);
+
+        PersonVo person06 = new PersonVo(4,"이정재","010-9999-9999","02-9999-9999");
+        phoneDao.personUpdate(person06);
+
+        phoneDao.personDelete(5);
+
+    }
 }
--- src/com/javaex/phone/PhoneDao.java
+++ src/com/javaex/phone/PhoneDao.java
@@ -1,5 +1,150 @@
 package com.javaex.phone;
 
-public class PhoneDao {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class PhoneDao {
+    private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private String driver = "oracle.jdbc.driver.OracleDriver";
+    private String id = "phonedb";
+    private String pw = "phonedb";
+
+    private Connection conn = null;
+    private PreparedStatement pstmt = null;
+    private ResultSet rs = null;
+
+    public void phoneDao() {
+    }
+
+    private void getConnect() {
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, id, pw);
+            System.out.println("DB 접속성공");
+        } catch (ClassNotFoundException e) {
+            System.out.println("error: 드라이버 로딩 실패 - " + e);
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        }
+    }
+
+    private void closeConnect() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        }
+    }
+
+    public List<PersonVo> getPersonList() {
+        getConnect();
+        List<PersonVo> personList = new ArrayList<PersonVo>();
+
+        try {
+            String personSelectQuery =
+                    "select  person_id," +
+                            "        name," +
+                            "        hp," +
+                            "        company " +
+                            "from    person";
+            pstmt = conn.prepareStatement(personSelectQuery);
+            rs = pstmt.executeQuery();
+            // 4.결과처리
+            while (rs.next()) {
+                int personId = rs.getInt("person_id");
+                String name = rs.getString("name");
+                String hp = rs.getString("hp");
+                String company = rs.getString("company");
+
+                PersonVo personVo = new PersonVo(personId, name, hp, company);
+                personList.add(personVo);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        }
+
+        closeConnect();
+
+        return personList;
+    }
+
+    public void personInsert(PersonVo personVo) {
+        getConnect();
+
+        try {
+            String personInserQuery =
+                    "insert into person " +
+                            "values(seq_person_id.nextVal,?,?,?)";
+            pstmt = conn.prepareStatement(personInserQuery);
+            pstmt.setString(1, personVo.getName());
+            pstmt.setString(2, personVo.getHp());
+            pstmt.setString(3, personVo.getCompany());
+
+            int count = pstmt.executeUpdate();
+
+            System.out.println(count + "건 입력되었습니다.");
+
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        }
+
+        closeConnect();
+    }
+
+    public void personUpdate(PersonVo personVo) {
+        getConnect();
+
+        try {
+            String personUpdateQuery =
+                    "update person " +
+                            "set name = ?, " +
+                            "    hp = ?,  " +
+                            "    company = ? " +
+                            "where person_id = ?  "; //?는 나중에 채움
+            pstmt = conn.prepareStatement(personUpdateQuery); //쿼리문 준비
+            pstmt.setString(1, personVo.getName());
+            pstmt.setString(2, personVo.getHp());
+            pstmt.setString(3, personVo.getCompany());
+            pstmt.setInt(4, personVo.getPersonId());
+
+            int count = pstmt.executeUpdate(); //쿼리문 날린다!!!!!!
+            // 4.결과처리
+            System.out.println(count + "건 업데이트 되었습니다.");
+
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        }
+
+        closeConnect();
+    }
+
+    public void personDelete(int personId) {
+        getConnect();
+
+        try {
+            String personDeleteQuery =
+                    "delete person " +
+                            "where person_id = ? "; //?는 나중에 채움
+            pstmt = conn.prepareStatement(personDeleteQuery); //쿼리문 준비
+            pstmt.setInt(1, personId); //4번을 지운다
+            int count = pstmt.executeUpdate(); //쿼리문 날린다!!!!!!
+            // 4.결과처리
+            System.out.println(count + "건 삭제되었습니다.");
+
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        }
+
+        closeConnect();
+    }
 }
